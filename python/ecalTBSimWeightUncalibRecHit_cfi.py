496169ad2f477d1872bbc724e2a47a41a2e8a014
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-04-25 13:40:00 +0000

    Initial changes for pileup memory reduction

--- python/ecalTBSimWeightUncalibRecHit_cfi.py
+++ python/ecalTBSimWeightUncalibRecHit_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # producer of rechits starting from tb simulation
 ecalTBSimWeightUncalibRecHit = cms.EDProducer("EcalTBWeightUncalibRecHitProducer",
-    EBdigiCollection = cms.InputTag('simEcalUnsuppressedDigis',''),
+    EBdigiCollection = cms.InputTag('mix', 'simEcalUnsuppressedDigis'),
     EEdigiCollection = cms.InputTag('',''),                                              
     tdcRecInfoCollection = cms.InputTag('ecalTBSimTDCReconstructor','EcalTBTDCRecInfo'),
     EBhitCollection = cms.string('EcalUncalibRecHitsEB'),
