6fd751330a9ad3a7f9defd267cb487d344f792b7
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-06-06 21:08:43 +0000

    back out change made obsolete by EDAlias feature

--- python/ecalTBSimWeightUncalibRecHit_cfi.py
+++ python/ecalTBSimWeightUncalibRecHit_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # producer of rechits starting from tb simulation
 ecalTBSimWeightUncalibRecHit = cms.EDProducer("EcalTBWeightUncalibRecHitProducer",
-    EBdigiCollection = cms.InputTag('mix', 'simEcalUnsuppressedDigis'),
+    EBdigiCollection = cms.InputTag('simEcalUnsuppressedDigis',''),
     EEdigiCollection = cms.InputTag('',''),                                              
     tdcRecInfoCollection = cms.InputTag('ecalTBSimTDCReconstructor','EcalTBTDCRecInfo'),
     EBhitCollection = cms.string('EcalUncalibRecHitsEB'),
